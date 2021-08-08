07207a0138f79e890a6d59fb57f9c7735bceb2f9
alicepfrieger <85840996+alicepfrieger@users.noreply.github.com>
2021-08-08 19:00:11 +0200

    Trying to make the hamburger menu run

---
+++ script.js
@@ -0,0 +1,13 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $(".hamburger-button").click(function() {
+        $(this).toggleClass("active");
+        $(".mobile-menu").fadeToggle()
+    });
+
+    AOS.init();
+
+
+
+});
